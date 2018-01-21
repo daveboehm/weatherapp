87cfdb99d08de52efe88252eaf1f1b1475a3cfe3
Dave Boehm <davidnboehm@gmail.com>
2018-01-21 17:48:27 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+body, html {
+  margin: 0;
+  padding: 0;
+  background-color: #fff;
+}
+</style>
