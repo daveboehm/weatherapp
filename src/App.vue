8ae9342c2fad350622321995460ce6cca20534cf
Dave Boehm <davidnboehm@gmail.com>
2018-02-10 17:04:50 -0600

    wip

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,25 @@
   <div id="app">
     <router-view/>
     <label>
-      <input type="text" v-model="city" />
-      <button @click="getCityCoords()">Get Weather</button>
+      <input type="text" v-model="searchLocation.city" />
+      <button @click="onCitySearch()">City Search</button>
     </label>
-    <p>{{currentWeather.data.currently.temperature || 'Loading'}}</p>
+    <p>Current Temperate at this computer's location: {{currentWeather.data.currently.temperature || 'Loading'}}</p>
+    <ul class="searchedCity">
+      <li class="cityInformation">
+        <p>{{searchLocation.city}}</p>
+        <p>{{searchLocation.temperature}}</p>
+        <p>{{searchLocation.chanceOfRain}}</p>
+        <button class="button" @click="onCityTrack">Track this City's Weather</button>
+      </li>
+    </ul>
+    <ul class="trackedCities">
+      <li v-for="(city,i) in trackedWeather" :key="i" class="cityInformation">
+        <p>{{city.city}}</p>
+        <p>{{city.temperature}}</p>
+        <p>{{city.chanceOfRain}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,53 +35,111 @@ export default {
   name: "App",
   data () {
     return {
-      location: {},
-      city: '',
       currentWeather: {
         data: {
-          currently: 0
+          currently: {
+            temperature: undefined
+          }
         }
-      }
+      },
+
+      // The tracked cities per user's local Storage
+      trackedWeather: [
+        {
+          city: 'Austin, TX',
+          temperature: '54 F',
+          chanceOfRain: '27%'
+        }, {
+          city: 'Madison, WI',
+          temperature: '11 F',
+          chanceOfRain: '0.5%'
+        }
+      ],
+      
+      // Placeholder for current location and local weather stats
+      userLocation: {
+        city: '',
+        lat: '',
+        lon: '',
+        temperature: '',
+        chanceOfRain: ''
+      },
+
+      // Placeholder for searched city's weather and stats
+      searchLocation: {
+        city: '',
+        coords: {},
+        temperature: '',
+        chanceOfRain: ''
+      },
     }
-  },
-  computed: {
-    
   },
   methods: {
-    getCityCoords() {
-      Geocoder.geocode(this.city, (err, data) => {
-          this.city = data.results[0].formatted_address;
-          this.lat = data.results[0].geometry.location.lat;
-          this.lon = data.results[0].geometry.location.lon;
-          this.getWeather()
+    getWeather(coords) {
+      console.log(coords)
+      Axios.get(`https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/${coords.lat},${coords.lon}`).then(weather => {
+        this.searchLocation.temperature = weather.currently.temperature;
       });
     },
-    getWeather(location) {
-      console.log(location);
-      
-    }
-  },
-  beforeMount() {
-    return new Promise( (resolve, reject) => {
+    getCurrentLocationWeather() {
       navigator.geolocation.getCurrentPosition( data => {
         let location = {
           lat: data.coords.latitude,
           lon: data.coords.longitude
         }
-        Axios.get(`https://api.darksky.net/forecast/acac1384f16e2dccd6b5813d915b0ae8/${location.lat},${location.lon}`).then( (weather) => {
+        Axios.get(`https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/${location.lat},${location.lon}`).then(weather => {
           this.currentWeather = weather;
+          console.log(weather)
         });
       });
+    },
+    onCitySearch($event) {
+      return new Promise( (resolve, reject) => {
+        Geocoder.geocode(this.searchLocation.city, function ( err, data ) {
+          this.searchLocation.city = data.results[0].formatted_address;
+          this.searchLocation.coords = data.results[0].geometry.location;
+        });
+        this.getWeather(this.searchLocation.coords)
+      });
+    },
+    getCityCoords() {
+      return new Promise( (resolve, reject) => {
+        Geocoder.geocode(this.city, (err, data) => {
+          this.city = data.results[0].formatted_address;
+          this.lat = data.results[0].geometry.location.lat;
+          this.lon = data.results[0].geometry.location.lon;
+          
+        });
+      }).then( () => {
+        this.getWeather(this.userLocation);
+      })
       
+    },
+    getWeather(location) {
+      console.log(location);
+      
+    },
+    onCityTrack($event) {
+      console.log('set this shit to local storage');
+    }
+  },
+  beforeMount() {
+    return new Promise( (resolve, reject) => {
+      this.getCurrentLocationWeather();
       resolve();
     });
-    
   }
   
 };
 </script>
 
 <style lang="scss">
+
+.trackedCity {
+  padding: 20px;
+  margin: 20px;
+  border: 1px solid #d9d9d9; 
+}
 
 
 </style>
